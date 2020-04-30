f28c534827b85efeaa0c285c1da27b9b9462b262
◱ PixelyIon <pixelyion@protonmail.com>
2020-04-30 21:53:39 +0530

    Refactor C++ Input
    This commit refactors the C++ end of Input so it'll be in line with the rest of the codebase and be ready for the extension with multiple players and controller configuration.

--- app/src/main/java/emu/skyline/EmulationActivity.kt
+++ app/src/main/java/emu/skyline/EmulationActivity.kt
@@ -133,10 +133,9 @@ class EmulationActivity : AppCompatActivity(), SurfaceHolder.Callback {
 
         setContentView(R.layout.emu_activity)
 
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
 
